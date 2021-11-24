93df1387c7d77e33258b45f26bedd00eb7f0cdc1
mmfarzaneh <mahdi6989@gmail.com>
2021-11-24 10:09:14 +0330

    Second Commit

---
+++ app/src/main/java/ir/ucrop/callback/CropBoundsChangeListener.java
@@ -0,0 +1,10 @@
+package ir.ucrop.callback;
+
+/**
+ * Interface for crop bound change notifying.
+ */
+public interface CropBoundsChangeListener {
+
+    void onCropAspectRatioChanged(float cropRatio);
+
+}
